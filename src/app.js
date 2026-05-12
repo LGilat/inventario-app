b2dd300b815573f58d125ee691b0bebf11202603
Lgilat <scho1636@gmail.com>
2026-05-12 10:45:47 +0200

    add frontend vercel domain to cors

--- src/app.js
+++ src/app.js
@@ -1,36 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const clienteRoutes = require('./routes/cliente.routes');
-const proveedorRoutes = require('./routes/proveedor.routes');
-const productoRoutes = require('./routes/producto.routes');
-const pedidoCompraRoutes = require('./routes/pedidoCompra.routes');
-const detallePedidoRoutes = require('./routes/detallePedido.routes');
-const stockRoutes = require('./routes/stock.routes');
-const ventaRoutes = require('./routes/venta.routes');
+const clienteRoutes = require("./routes/cliente.routes");
+const proveedorRoutes = require("./routes/proveedor.routes");
+const productoRoutes = require("./routes/producto.routes");
+const pedidoCompraRoutes = require("./routes/pedidoCompra.routes");
+const detallePedidoRoutes = require("./routes/detallePedido.routes");
+const stockRoutes = require("./routes/stock.routes");
+const ventaRoutes = require("./routes/venta.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5174",
+      "https://inventario-frontend.vercel.app",
+    ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    headers: ['Content-Type', 'Authorization'],
-  })
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    headers: ["Content-Type", "Authorization"],
+  }),
 );
 
-app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
+app.get("/", (req, res) => {
+  res.send("¡Hola Mundo!");
 });
 
-app.use('/api', clienteRoutes);
-app.use('/api', proveedorRoutes);
-app.use('/api', productoRoutes);
-app.use('/api', pedidoCompraRoutes);
-app.use('/api', detallePedidoRoutes);
-app.use('/api', stockRoutes);
-app.use('/api', ventaRoutes);
+app.use("/api", clienteRoutes);
+app.use("/api", proveedorRoutes);
+app.use("/api", productoRoutes);
+app.use("/api", pedidoCompraRoutes);
+app.use("/api", detallePedidoRoutes);
+app.use("/api", stockRoutes);
+app.use("/api", ventaRoutes);
 
 module.exports = app;
